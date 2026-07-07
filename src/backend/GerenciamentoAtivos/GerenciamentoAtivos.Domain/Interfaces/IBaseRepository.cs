109c66aadb2ef914aad4e25c452824ff91cd4fe1
MarcelDev01 <developer.marcel01@gmail.com>
2026-07-07 10:39:56 -0300

    feat: adicionando interfaces do repositorio

---
+++ src/backend/GerenciamentoAtivos/GerenciamentoAtivos.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using GerenciamentoAtivos.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciamentoAtivos.Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<int> SaveChangesAsync();
+    }
+}
